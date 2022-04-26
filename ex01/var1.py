88f9700e095a1c4edfa2d4fa74a25aa4742cb370
tjdgus7562 <tjdgus7562@naver.com>
2022-04-26 14:10:01 +0900

    파이썬 타입

---
+++ ex01/var1.py
@@ -0,0 +1,43 @@
+# 파이썬 변수
+
+# 정수
+n1 = 1
+
+# 실수
+n2 = 1.1
+
+# 문자열
+s1 = '문자'
+
+# 긴문자열(홑따움표나 쌍따음표 세개씩)
+s2 = '''안녕하세요
+적당히 바람이 시원해
+기분이 너무 좋아요 
+유후'''
+
+# 문자 안에 쌍따옴표 추가
+s3= '그는 말했다. 왈 "왈왈...."라고'
+
+# 문자 안에 변수 바인딩 f-str
+username = '하준서이'
+s4 = f'안녕 내 이름은 {username}야'
+
+print(n1)
+print(n2)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+print('='*30)
+
+print(type(n1))
+print(type(n2))
+print(type(s1))
+print(type(s2))
+
+print('='*30)
+
+print(n1,n2,s1,s2,s3,s4)
+
+
